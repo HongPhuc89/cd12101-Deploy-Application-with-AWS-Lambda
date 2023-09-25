ca171577bffb56a099f747f884d297772027185f
Nguyen Hong Phuc <scottfu89@gmail.com>
2023-09-25 14:02:10 +0700

    Update get Todo API

--- starter/backend/src/lambda/http/getTodos.js
+++ starter/backend/src/lambda/http/getTodos.js
@@ -1,4 +1,26 @@
+import { DynamoDB } from '@aws-sdk/client-dynamodb'
+import { DynamoDBDocument } from '@aws-sdk/lib-dynamodb'
+
+const dynamoDbClient = DynamoDBDocument.from(new DynamoDB())
+
+const todosTable = process.env.TODOS_TABLE
+
 export function handler(event) {
   // TODO: Get all TODO items for a current user
-  return undefined
+  console.log('Processing event: ', event)
+  const scanCommand = {
+    TableName: todosTable
+  }
+  const result = await dynamoDbClient.scan(scanCommand)
+  const items = result.Items
+
+  return {
+    statusCode: 200,
+    headers: {
+      'Access-Control-Allow-Origin': '*'
+    },
+    body: JSON.stringify({
+      items
+    })
+  }
 }
