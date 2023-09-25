83143e093eeabf041e7849083f18be449eb60cfc
Nguyen Hong Phuc <scottfu89@gmail.com>
2023-09-25 15:50:58 +0700

    Update create Todo API

--- starter/backend/src/lambda/http/createTodo.js
+++ starter/backend/src/lambda/http/createTodo.js
@@ -1,8 +1,40 @@
+import { DynamoDB } from '@aws-sdk/client-dynamodb'
+import { DynamoDBDocument } from '@aws-sdk/lib-dynamodb'
+import { v4 as uuidv4 } from 'uuid'
 
-export function handler(event) {
-  const newTodo = JSON.parse(event.body)
+import { getUserId } from '../utils.mjs'
 
-  // TODO: Implement creating a new TODO item
-  return undefined
+const dynamoDbClient = DynamoDBDocument.from(new DynamoDB())
+
+const groupsTable = process.env.TODOS_TABLE
+
+export async function handler(event) {
+  console.log('Processing event: ', event)
+  const userId = getUserId(event)
+  const itemId = uuidv4()
+
+  const parsedBody = JSON.parse(event.body)
+
+  const newItem = {
+    id: itemId,
+    userId: userId,
+    createdAt: new Date().toISOString(),
+    done: false,
+    ...parsedBody
+  }
+
+  await dynamoDbClient.put({
+    TableName: groupsTable,
+    Item: newItem
+  })
+
+  return {
+    statusCode: 201,
+    headers: {
+      'Access-Control-Allow-Origin': '*'
+    },
+    body: JSON.stringify({
+      newItem
+    })
+  }
 }
-
