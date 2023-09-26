c1ef7f80bbf7f5e13430ab66fb3834ae6316a968
Nguyen Hong Phuc <scottfu89@gmail.com>
2023-09-26 14:37:05 +0700

    Update delete Todo API

--- starter/backend/src/lambda/http/deleteTodo.js
+++ starter/backend/src/lambda/http/deleteTodo.js
@@ -1,8 +1,32 @@
+import { getUserId } from '../utils.mjs'
+import { DynamoDB } from '@aws-sdk/client-dynamodb'
+import { DynamoDBDocument, DeleteCommand } from '@aws-sdk/lib-dynamodb'
 
-export function handler(event) {
+const dynamoDbClient = DynamoDBDocument.from(new DynamoDB())
+
+const todosTable = process.env.TODOS_TABLE
+
+export async function handler(event) {
+  console.log('Processing event: ', event)
+  const userId = getUserId(event)
   const todoId = event.pathParameters.todoId
 
-  // TODO: Remove a TODO item by id
-  return undefined
-}
+  const params = {
+    TableName: todosTable,
+    Key: {
+      todoId: todoId,
+      userId: userId
+    }
+  }
 
+  await dynamoDbClient.send(new DeleteCommand(params))
+
+  return {
+    statusCode: 200,
+    headers: {
+      'Access-Control-Allow-Origin': '*',
+      'Access-Control-Allow-Credentials': true
+    },
+    body: JSON.stringify({ todoId: todoId })
+  }
+}
