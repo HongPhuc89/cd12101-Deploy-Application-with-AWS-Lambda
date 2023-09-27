bc54504772aa08d112d4ceb276899e99108c5ffb
Nguyen Hong Phuc <scottfu89@gmail.com>
2023-09-27 13:55:05 +0700

    Update getSignedUrl API

--- starter/backend/src/lambda/http/generateUploadUrl.js
+++ starter/backend/src/lambda/http/generateUploadUrl.js
@@ -1,7 +1,26 @@
-export function handler(event) {
-  const todoId = event.pathParameters.todoId
+import { PutObjectCommand, S3Client } from '@aws-sdk/client-s3'
+import { getSignedUrl } from '@aws-sdk/s3-request-presigner'
+const s3Client = new S3Client({region: process.env.S3_REGION})
+const bucketName = process.env.ATTACHMENT_S3_BUCKET 
 
-  // TODO: Return a presigned URL to upload a file for a TODO item with the provided id
-  return undefined
+export async function handler(event) {
+  const todoId = event.pathParameters.todoId
+  const command = new PutObjectCommand({
+    Bucket: bucketName,
+    Key: todoId
+  })
+
+  const signedUrl = await getSignedUrl(s3Client, command,  { expiresIn: 3600 });
+
+  return {
+    statusCode: 200,
+    headers: {
+      'Access-Control-Allow-Origin': '*',
+      'Access-Control-Allow-Credentials': true
+    },
+    body: JSON.stringify({
+      uploadUrl: signedUrl
+    })
+  }
 }
 
