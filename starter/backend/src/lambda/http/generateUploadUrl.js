fab278f19ee0bce88b4f776efbfc60fab8c80825
Nguyen Hong Phuc <scottfu89@gmail.com>
2023-09-27 17:24:01 +0700

    Update generate URL API

--- starter/backend/src/lambda/http/generateUploadUrl.js
+++ starter/backend/src/lambda/http/generateUploadUrl.js
@@ -1,9 +1,16 @@
+import { getUserId } from '../utils.mjs'
 import { PutObjectCommand, S3Client } from '@aws-sdk/client-s3'
 import { getSignedUrl } from '@aws-sdk/s3-request-presigner'
+import { DynamoDB } from '@aws-sdk/client-dynamodb'
+import { DynamoDBDocument, UpdateCommand } from '@aws-sdk/lib-dynamodb'
+
+const dynamoDbClient = DynamoDBDocument.from(new DynamoDB())
 const s3Client = new S3Client({region: process.env.S3_REGION})
-const bucketName = process.env.ATTACHMENT_S3_BUCKET 
+const bucketName = process.env.ATTACHMENT_S3_BUCKET
+const todosTable = process.env.TODOS_TABLE
 
 export async function handler(event) {
+  const userId = getUserId(event)
   const todoId = event.pathParameters.todoId
   const command = new PutObjectCommand({
     Bucket: bucketName,
@@ -11,6 +18,22 @@ export async function handler(event) {
   })
 
   const signedUrl = await getSignedUrl(s3Client, command,  { expiresIn: 3600 });
+  const params = {
+    TableName: todosTable,
+    Key: {
+      todoId: todoId,
+      userId: userId
+    },
+    ExpressionAttributeNames: {
+      '#attachmentUrl': 'attachmentUrl'
+    },
+    UpdateExpression: 'set #attachmentUrl = :attachmentUrl',
+    ExpressionAttributeValues: {
+      ':attachmentUrl': signedUrl,
+    }
+  }
+
+  await dynamoDbClient.send(new UpdateCommand(params))
 
   return {
     statusCode: 200,
